0fa9df13a091b4256fa2dd90de881e0116369dbc
AlymkanKalykova <alymkan23@gmail.comgit config --global user.name >
2024-07-29 17:07:20 +0600

    отредактировала модель комментариев

--- source/webapp/models/comment.py
+++ source/webapp/models/comment.py
@@ -1,13 +1,19 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 from base_model import BaseModel
 
 
 class Comment(BaseModel):
-    article = models.ForeignKey('webapp.Article', related_name='comments', on_delete=models.CASCADE,
+    task = models.ForeignKey('webapp.Task', related_name='comments', on_delete=models.CASCADE,
                                 verbose_name='Статья')
-    text = models.TextField(max_length=400, verbose_name='Комментарий')
-    author = models.CharField(max_length=40, null=True, blank=True, default='Аноним', verbose_name='Автор')
+    comment = models.TextField(max_length=400, verbose_name='Комментарий')
+    author = models.ForeignKey(
+        get_user_model(),
+        related_name='comments',
+        on_delete=models.SET_DEFAULT,
+        default=1
+    )
 
     def __str__(self):
         return self.text[:20]
